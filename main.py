f9f77e57ccbe766b6ecf26b3f6b58cd0cc6af0e5
Dawei Hao <dwsamurai84@gmail.com>
2024-10-16 08:07:31 +0800

    Add content.

--- main.py
+++ main.py
@@ -6,3 +6,30 @@
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+        
+# for name in names:
+all_the_names = []
+# 找到Absolute File Path.
+names = open("/Users/haodawei/Library/Mobile Documents/com~apple~CloudDocs/PyCharm Projects/Day-24-Mail-Merge/Input/Names/invited_names.txt", "r")
+print(names.readline())
+
+# for-Loop整個names。
+for name in names:
+    name = name.strip()
+    print(name)
+    all_the_names.append(name)
+
+# 得到 ['Zuko', 'Appa', 'Katara', 'Sokka', 'Momo', 'Uncle Iroh', 'Toph']
+print(all_the_names)
+
+# 把all_the_names的內容 取代 starting_letter.txt。
+mail = open("/Users/haodawei/Library/Mobile Documents/com~apple~CloudDocs/PyCharm Projects/Day-24-Mail-Merge/Input/Letters/starting_letter.txt", "r")
+mail_content = mail.readline()
+print(mail_content)
+
+replaced_TEXT = "[name]"
+#
+# for new_sample in all_the_names:
+
+
+
