bbb2ab5e09e4a58a9bfe16ebeebbcddb8a07f765
Dawei Hao <dwsamurai84@gmail.com>
2024-10-16 22:14:48 +0800

    Add content, and replaced the new heading.

--- main.py
+++ main.py
@@ -24,12 +24,16 @@ print(all_the_names)
 
 # 把all_the_names的內容 取代 starting_letter.txt。
 mail = open("/Users/haodawei/Library/Mobile Documents/com~apple~CloudDocs/PyCharm Projects/Day-24-Mail-Merge/Input/Letters/starting_letter.txt", "r")
-mail_content = mail.readline()
+mail_content = mail.read()
 print(mail_content)
 
 replaced_TEXT = "[name]"
-#
-# for new_sample in all_the_names:
 
-
+# saved data.
+for i in all_the_names:
+    revised_content = mail_content.replace(replaced_TEXT, i)
+    print(revised_content)
+    # Write each data by using mode w to saved it into the new file.
+    with open(f"./Output/ReadyToSend/letter_for_{ i }.docx",mode="w") as completed_letter:
+        completed_letter.write(revised_content)
 
